27f530d11c1b66ebd23cc00d730ee0613ccf43c4
Renato Cavalcante Silva <renatofh@hotmail.com>
2026-05-13 18:55:48 -0300

    Estrutura inicial backend e frontend

---
+++ AiNumera.Api/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiNumera.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Dashboard data goes here." });
+        }   
+    }
+}
